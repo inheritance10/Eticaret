68f0861134e5e65dcbf54cca2d23bd31943c5bd6
inheritance10 <ali552001a@gmail.com>
2022-06-12 15:39:58 +0300

    kategori,urunlerin model,migration, işlemleri yapıldı.Kategorilerin listelenmesi ve alt kategorilendirme tamamlandı.

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class AnasayfaController extends Controller
 {
-    //
+    public function index(){
+        $kategoriler = Kategori::whereRaw('ust_id is null')->get()->take(8);
+        return view('anasayfa',compact('kategoriler'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AnasayfaController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UrunController;
+use App\Http\Controllers\SepetController;
+use App\Http\Controllers\OdemeController;
+use App\Http\Controllers\SiparisController;
+use App\Http\Controllers\KullaniciController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +18,47 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+Route::get('anasayfa',[AnasayfaController::class,'index'])
+    ->name('anasayfa.index');
+
+//KATEGORİ ROUTE TANIMLAMASI
+Route::get('kategori/{slug_kategoriadi}',[KategoriController::class,'index'])
+    ->name('kategori.index');
+
+Route::get('urun/{slug_urunadi}',[UrunController::class,'index'])
+    ->name('urun.index');
+
+Route::get('sepet',[SepetController::class,'index'])
+    ->name('sepet.index');
+
+Route::get('odeme',[OdemeController::class,'index'])
+    ->name('odeme.index');
+
+Route::get('siparisler',[SiparisController::class,'index'])
+    ->name('siparisler.index');
+
+Route::get('siparisler/{id}',[SiparisController::class,'siparisDetay'])
+    ->name('siparis-detay.index');
+
+Route::prefix('kullanici')->group(function (){
+
+    Route::get('girisyap',[KullaniciController::class,'girisform'])
+        ->name('girisform.index');
+
+    Route::get('kayitol',[KullaniciController::class,'kayitform'])
+        ->name('kayitform.index');
+});
+
+
+
+
+
+
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
